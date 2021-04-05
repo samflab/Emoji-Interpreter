59a5955d110457d1baeab648c18f317c0660beb1
samflab <kasturimeh@gmail.com>
2021-04-05 12:49:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -2,22 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function textfieldHandler(event) {
+    console.log(event.target.value);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Emoji Interpreter</h1>
+      <input onChange={(event) => textfieldHandler(event)} />
+    
     </div>
   );
 }
