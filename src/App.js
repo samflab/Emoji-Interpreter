1a1ec98d97d2bee77674b02891c0cf03420c020e
Masudha Meher <55777560+samflab@users.noreply.github.com>
2021-04-07 11:43:40 +0530

    added onclick event

--- src/App.js
+++ src/App.js
@@ -7,18 +7,48 @@ var emojiDictionary = {
   "🐁": "Animal rat"
 };
 
+var emojisWeHave = Object.keys(emojiDictionary);
 function App() {
   var [meaning, setMeaning] = useState("");
   function emojiInputHandler(event) {
     var userEmojiInput = event.target.value;
     var meaning = emojiDictionary[userEmojiInput];
+
+    if (meaning === undefined) meaning = "We don't have this in our db";
+
     setMeaning(meaning);
   }
+
+  function emojiClickHandler(emoji) {
+    var meaning = emojiDictionary[emoji];
+    setMeaning(meaning);
+  }
+
   return (
     <div className="App">
       <h1>Emoji Interpreter</h1>
-      <input onChange={(event) => emojiInputHandler(event)} />
-      <h2>{meaning} </h2>
+      <input onChange={emojiInputHandler} />
+      <h2>Meaning of the Emoji: {meaning} </h2>
+      <h4> Emojis We Currently Have </h4>
+      {
+        <span>
+          {emojisWeHave.map(function (emoji) {
+            return (
+              <span
+                onClick={() => emojiClickHandler(emoji)}
+                key={emoji}
+                style={{
+                  fontSize: "1.5rem",
+                  padding: ".9%",
+                  cursor: "pointer"
+                }}
+              >
+                {emoji}
+              </span>
+            );
+          })}
+        </span>
+      }
     </div>
   );
 }
